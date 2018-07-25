2b095065eb9cdff2841e63a8c4a1751a645c3439
Mike Mugge <git@idaz.co>
2018-07-25 16:37:41 -0600

    [IMP] add extra-addons

--- 10/build.sh
+++ 10/build.sh
@@ -22,11 +22,38 @@ elif [ ! -f download/odoo.deb ]; then
 	cd ../
 fi
 
+read -p "Download (or re-download) extra addons? (y/N) " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	# Download selected add-ons from other projects
+	function clean_up () {
+		rm -rf ./download/addons/tmp*
+	}
+	mkdir -p ./download/addons/selected
+	clean_up
+	function get_zip_file_from_github () {
+		curl -L -o ./download/addons/tmp.zip $1
+		unzip ./download/addons/tmp.zip -d ./download/addons/tmp
+	}
+	function clean_mv () {
+		rm -rf ./download/addons/selected/$2
+		mv ./download/addons/tmp$1/$2 ./download/addons/selected/$2
+	}
+	# from https://github.com/it-projects-llc/misc-addons
+	URL="https://github.com/it-projects-llc/misc-addons/archive/10.0.zip"
+	get_zip_file_from_github $URL
+	clean_mv /misc-addons-10.0 base_session_store_psql
+	clean_mv /misc-addons-10.0 ir_attachment_s3
+	clean_mv /misc-addons-10.0 web_debranding
+	# TODO: add other modules as needed
+	clean_up
+fi
+
+
 # Build the image
 echo "Building image"
 
 read -p "Disable build cache? (y/N) " -n 1 -r
-echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     NO_BUILD_CACHE="--no-cache=true"
