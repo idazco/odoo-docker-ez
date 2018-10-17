3363f377e698609de79f0662140ec07548582c6a
Mike Mugge <git@idaz.co>
2018-10-17 13:52:02 -0600

    use module from other lib

--- 10/build.sh
+++ 10/build.sh
@@ -3,31 +3,48 @@ cd "$(dirname "$0")"
 
 mkdir -p ./download/addons/selected
 
-read -p "Download (or re-download) extra addons? (y/N) " -n 1 -r
+
+#========
+# Download selected add-ons from other projects
+function clean_up () {
+	rm -rf ./download/addons/tmp*
+}
+clean_up
+function get_zip_file_from_github () {
+	curl -L -o ./download/addons/tmp.zip $1
+	unzip ./download/addons/tmp.zip -d ./download/addons/tmp
+}
+function clean_mv () {
+	rm -rf ./download/addons/selected/$2
+	mv ./download/addons/tmp$1/$2 ./download/addons/selected/$2
+}
+#========
+
+
+# https://github.com/it-projects-llc
+read -p "Get / refresh addons from https://github.com/it-projects-llc? (y/N) " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	# Download selected add-ons from other projects
-	function clean_up () {
-		rm -rf ./download/addons/tmp*
-	}
-	clean_up
-	function get_zip_file_from_github () {
-		curl -L -o ./download/addons/tmp.zip $1
-		unzip ./download/addons/tmp.zip -d ./download/addons/tmp
-	}
-	function clean_mv () {
-		rm -rf ./download/addons/selected/$2
-		mv ./download/addons/tmp$1/$2 ./download/addons/selected/$2
-	}
-	# from https://github.com/it-projects-llc/misc-addons
 	URL="https://github.com/it-projects-llc/misc-addons/archive/10.0.zip"
 	get_zip_file_from_github $URL
-	clean_mv /misc-addons-10.0 base_session_store_psql
 	clean_mv /misc-addons-10.0 web_debranding
-	# TODO: add other modules as needed
+	# TODO: copy other modules as needed
 	clean_up
 	echo
 fi
+
+
+# https://github.com/odoo/odoo-extra
+read -p "Get / refresh addons from https://github.com/odoo/odoo-extra? (y/N) " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	URL="https://github.com/odoo/odoo-extra/archive/10.0.zip"
+	get_zip_file_from_github $URL
+	clean_mv /odoo-extra-10.0 session_db
+	clean_up
+	echo
+fi
+
 
 read -p "Disable build cache? (y/N) " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
