845b25e0c0f18aaa9297415b85112facbe369580
Mike Mugge <git@idaz.co>
2020-03-24 15:28:08 -0600

    start nginx by default for 10

--- 10/entrypoint.sh
+++ 10/entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 # start nginx as reverse proxy for Odoo
-function start_proxy() {
-	nginx &
-	sleep 5
-	echo "######################"
-	echo "Checking proxy service"
-	echo "######################"
-	curl --fail http://127.0.0.1:8080/proxy-health || exit 1
-}
+nginx &
+sleep 5
+echo "######################"
+echo "Checking proxy service"
+echo "######################"
+curl --fail http://127.0.0.1:8080/proxy-health || exit 1
 
 # set the postgres database host, port, user and password according to the environment
 # and pass them as arguments to the odoo process if not present in the config file
@@ -44,7 +42,6 @@ case "$1" in
         exec odoo "$@" "${DB_ARGS[@]}"
         ;;
     *)
-    	start_proxy
         exec "$@"
 esac
 
